0a7938b23304fc3102b08e606c36d9228d92d4d5
Dan Dudaiev <roffrooff@gmail.com>
2023-04-14 16:53:35 +0300

    pre-refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
+import { FeedbackStatistic } from "./feedback-statistics/FeedbackStatistic";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <FeedbackStatistic/>
   );
 };
